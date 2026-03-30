9b78cb251566971d3ef2694206cf7d4874fe2be2
pc35559 <pc35559@uga.edu>
2026-03-30 09:05:26 -0400

    Added new logic to display certain heading buttons when logged in vs. out.

--- ecinemabookingsys/src/components/Header.tsx
+++ ecinemabookingsys/src/components/Header.tsx
@@ -1,12 +1,26 @@
-import { Film, Ticket, User, LogIn } from 'lucide-react';
-import { Link, useLocation } from 'react-router';
+import { Film, Ticket, User, LogIn, LogOut } from 'lucide-react';
+import { Link, useLocation, useNavigate } from 'react-router';
 
 export function Header() {
   const location = useLocation();
+  const navigate = useNavigate();
+
+  //Check if the user is currently logged in
+  const userString = sessionStorage.getItem('user');
+  //If the user exists in the current session, assign it to the user constant. It stays null if the user is not found.
+  const user = userString ? JSON.parse(userString) : null;
   
   const isActive = (path: string) => {
     return location.pathname === path;
   };
+
+  //Handle the logout
+  const handleLogout = () => {
+    sessionStorage.clear();
+    navigate('/');
+    //Refresh the page upon logout (feels reassuring when it refreshes)
+    window.location.reload();
+  }; //handleLogout
 
   return (
     <header className="bg-gray-900 text-white shadow-lg">
@@ -17,6 +31,7 @@ export function Header() {
             <span>Cinema E-Booking System</span>
           </Link>
           
+          {/*the Movies tab is available regardless if a user is logged in or not*/}
           <nav className="flex items-center gap-6">
             <Link
               to="/"
@@ -27,20 +42,42 @@ export function Header() {
               <Film className="w-5 h-5" />
               <span>Movies</span>
             </Link>
-<<<<<<< HEAD
-=======
 
->>>>>>> 8be98aec0653c67effbcb9c085b185255649a10d
-            <Link
-              to="/bookings"
-              className={`flex items-center gap-2 hover:text-red-400 transition-colors ${
-                isActive('/bookings') ? 'text-red-400' : ''
-              }`}
-            >
-              <Ticket className="w-5 h-5" />
-              <span>My Bookings</span>
-            </Link>
-  
+            {user ? (
+              <>
+              {/*These only appear when the user is logged in: Movies, My Bookings, Profile, Logout*/}
+              <Link
+                to="/bookings"
+                className={`flex items-center gap-2 hover:text-red-400 transition-colors ${
+                  isActive('/bookings') ? 'text-red-400' : ''
+                }`}
+              >
+                <Ticket className="w-5 h-5" />
+                <span>My Bookings</span>
+              </Link>
+
+              <Link
+                to="/profile"
+                className={`flex items-center gap-2 hover:text-red-400 transition-colors ${
+                  isActive('/profile') ? 'text-red-400' : ''
+                }`}
+              >
+                <User className="w-5 h-5" />
+                <span>Profile</span>
+              </Link>
+
+              <button onClick={handleLogout} 
+                className={`flex items-center gap-2 hover:text-red-400 transition-colors ${
+                  isActive('/profile') ? 'text-red-400' : ''
+                  }`}
+                >
+                  <LogOut className="w-5 h-5"/>
+                  <span>Log Out</span>
+                </button>
+              </>
+            ) : (
+            <>
+            {/*These only appear when the user is logged out: Movies, Login*/}
             <Link
               to="/login"
               className={`flex items-center gap-2 hover:text-red-400 transition-colors ${
@@ -48,22 +85,11 @@ export function Header() {
               }`}
             >
             <LogIn className="w-5 h-5" />
-            <span>Login</span>
+            <span>Log In</span>
           </Link>
 
-
-          <Link
-              to="/register"
-              className={`flex items-center gap-2 hover:text-red-400 transition-colors ${
-                isActive('/register') ? 'text-red-400' : ''
-              }`}
-            >
-            <LogIn className="w-5 h-5" />
-            <span>Register</span>
-          </Link>
-
-
-          
+          </>
+        )}
 
           </nav>
         </div>
